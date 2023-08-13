c1084f0f5a6bda0c192d54ed5699fd27e56613f0
christian <chmjits22@gmail.com>
2023-08-13 00:38:23 -0300

    my first commit

---
+++ spamm.py
@@ -0,0 +1,29 @@
+import pyautogui as pg
+import time 
+time.sleep(5)
+for i in range (50):
+ pg.write("jsassasasjajsajsjajajajajajjj")
+ pg.press('enter')
+#segundo metodo de spam con clase pyautogi
+# import pyautogui, webbrowser
+# from time import sleep 
+
+# webbrowser.open('https.//web.whatsapp.com/send?phone='+59893492294)
+# sleep(5)
+# for i in range (700):
+#      pyautogui.typewrite('puto podes ds?')
+#      pyautogui.press('enter') 
+# Import the relevant modules
+
+# tersera opcion  
+# import pyautogui
+# import time
+
+# # FAIL-SAFE - DO NOT DISABLE THIS
+# pyautogui.FAILSAFE = True
+
+# # Example
+# time.sleep(3)
+# text = open("text.txt")
+# for each_line in text:
+#     pyautogui.write(each_line)
